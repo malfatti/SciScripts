41b7afce0d47fc9f197cbcffb67f0e9d64893a7c
Thawann Malfatti <malfatti@neuro.ufrn.br>
2016-10-09 15:41:30 -0300

    Improved CutVideo.sh

--- Bash/CutVideo.sh
+++ Bash/CutVideo.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-Start=""; End=""
+Start="Empty"; End="Empty"
 
-while [[ $# -gt 1 ]]; do
-    key="$1"
-
-    case $key in
-        "--help")
-            echo "Usage: "$0" [FileName] [DurationInSeconds]"
+while getopts :v:s:e:h Option; do
+    case "${Option}" in
+        v)
+            Video=${OPTARG}
+        ;;
+        s)
+            Start=${OPTARG}
+        ;;
+        e)
+            End=${OPTARG}
+        ;;
+        h) 
+            echo -en '\n'
+            echo "Usage: CutVideo -v <FileName> [-s <DurationInSeconds> | -e <DurationInSeconds>]"
+            echo -en '\n'
+            
             exit 0
         ;;
-        "--video")
-            Video="$2"
-            shift 2
+        :)
+            echo "Option -"$OPTARG" needs a value. See 'CutVideo -h'."
+            exit 0
         ;;
-        "--start")
-            Start="$2"
-            shift 2
-        ;;
-        "--end")
-            End="$2"
-            shift 2
-        ;;
-        *)
-            echo "Usage: "$0" [FileName] [DurationInSeconds]"
+        \?)
+            echo "Unknown option "$OPTARG". See 'CutVideo -h'."
             exit 0
         ;;
     esac
@@ -31,13 +33,17 @@ done
 
 NewFileName="${Video%????}"-Cut."${Video: -3}"
 
-if [ "$End" == "" ]; then
-    ffmpeg -i "$Video" -ss "$Start" -c copy "$NewFileName"
-elif [ "$Start" == "" ]; then
-    ffmpeg -i "$Video" -to "$End" -c copy "$NewFileName"
-else
+if [[ "$Start" != "Empty" && "$End" != "Empty" ]]; then
     ffmpeg -i "$Video" -ss "$Start" -to "$End" -c copy "$NewFileName"
+    echo -en '\n'; echo -en '\n'; echo "Video saved as "$NewFileName""
+elif [[ "$Start" != "Empty" && "$End" == "Empty" ]]; then
+    ffmpeg -i "$Video" -ss "$Start" -c copy "$NewFileName"
+    echo -en '\n'; echo -en '\n'; echo "Video saved as "$NewFileName""
+elif [[ "$Start" == "Empty" && "$End" != "Empty" ]]; then
+    ffmpeg -i "$Video" -to "$End" -c copy "$NewFileName"
+    echo -en '\n'; echo -en '\n'; echo "Video saved as "$NewFileName""
+else
+    echo "Choose a start time or an end time. See 'CutVideo -h'."
 fi
 
-echo -en '\n'; echo -en '\n'; echo "Video saved as "$NewFileName""
 

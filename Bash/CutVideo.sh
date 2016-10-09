9b8191d0d8dd3071ff532be5ee1966dfa5f8a32b
Thawann Malfatti <malfatti@neuro.ufrn.br>
2016-10-09 09:30:56 -0300

    Improve CutVideo.sh

--- Bash/CutVideo.sh
+++ Bash/CutVideo.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-if [ "${1,,}" == --help ]; then
-    echo "Usage: CutVideo [FileName] [DurationInSeconds]"
-    exit 0
-fi
+Start=""; End=""
 
-Video="$1"
-Start="$2"
+while [[ $# -gt 1 ]]; do
+    key="$1"
+
+    case $key in
+        "--help")
+            echo "Usage: "$0" [FileName] [DurationInSeconds]"
+            exit 0
+        ;;
+        "--video")
+            Video="$2"
+            shift 2
+        ;;
+        "--start")
+            Start="$2"
+            shift 2
+        ;;
+        "--end")
+            End="$2"
+            shift 2
+        ;;
+        *)
+            echo "Usage: "$0" [FileName] [DurationInSeconds]"
+            exit 0
+        ;;
+    esac
+done
+
 NewFileName="${Video%????}"-Cut."${Video: -3}"
 
-ffmpeg -i "$Video" -ss "$Start" -c copy "$NewFileName"
+if [ "$End" == "" ]; then
+    ffmpeg -i "$Video" -ss "$Start" -c copy "$NewFileName"
+elif [ "$Start" == "" ]; then
+    ffmpeg -i "$Video" -to "$End" -c copy "$NewFileName"
+fi
+
 echo -en '\n'; echo -en '\n'; echo "Video saved as "$NewFileName""
+
--- Bash/Power/GoToSleep.sh
+++ Bash/Power/GoToSleep.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "Stopping Jack...";
-killall jackd &> /dev/null; killall alsa_in &> /dev/null
+#echo "Stopping Jack...";
+#killall jackd &> /dev/null; killall alsa_in &> /dev/null
 #echo "Stopping Bumblebee..."; killall bumblebeed
-echo "Stopping Alsa..."; sudo /etc/init.d/alsasound stop &> /dev/null
+#echo "Stopping Alsa..."; sudo /etc/init.d/alsasound stop &> /dev/null
 echo "Suspending..."; sudo pm-suspend
 
 echo "Waking..."
 echo "Locking screen..."; slock &
-echo "Starting Alsa..."; sudo /etc/init.d/alsasound start &> /dev/null
+#echo "Starting Alsa..."; sudo /etc/init.d/alsasound start &> /dev/null
 #echo "Starting Bumblebee..."; sudo bumblebeed &>> ~/Software/Git/Malfatti/SciScripts/Bash/BumblebeeSession.log
-echo "Starting Jack..."; bash ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/SoundAutostart.sh &> /dev/null
+#echo "Starting Jack..."; i
+#bash ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/SoundAutostart.sh &> /dev/null
 echo "Awake."

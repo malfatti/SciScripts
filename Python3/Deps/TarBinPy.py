e5a2c406e4896dcfed6c5e7cf54abbd695b67a7a
malfatti <malfatti@neuro.ufrn.br>
2017-01-21 13:53:03 -0200

    Done implementing Tar DataWrite

--- Python3/Deps/TarBinPy.py
+++ Python3/Deps/TarBinPy.py
@@ -7,12 +7,32 @@ Created on Wed Jan 18 10:28:45 2017
 """
 #%%
 from glob import glob
+
 import json
 import numpy as np
 import os
+import shutil
 import tarfile
 
-def DataRead(DataFile, InfoFile, ChList=[]):
+### Level 0 ###
+def DictRead(DictFile):
+    with open(DictFile, 'r') as File: Dict = json.load(File)
+    return(Dict)
+
+
+def DictWrite(DictFile, Dict):
+    with open(DictFile, 'w') as File: json.dump(Dict, File)
+    return(None)
+
+
+def TarWrite(TarFile, FileList):
+    with tarfile.open(TarFile, 'a') as F:
+        for File in FileList:
+            F.add(File)
+
+
+### Level 1 ###
+def BinRead(DataFile, Path, ChList=[]):
     """ Read flat interleaved binary data and return it as a numpy array. Data 
         will be represented as Data[Channels, Samples]. The ChList argument 
         allows to choose which channels will be loaded. If empty, all channels 
@@ -23,8 +43,9 @@ def DataRead(DataFile, InfoFile, ChList=[]):
         Info['DataDType']. """
     
     # Read Data and Info files
-    with open(DataFile, 'rb') as File: Raw = File.read()
-    Info = DictRead(InfoFile)
+    with open(Path + DataFile, 'rb') as File: Raw = File.read()
+    InfoFile = ''.join(DataFile.split('.')[:-1]) + '-Info.dict'
+    Info = DictRead(Path + InfoFile)
     
     # Convert bytes to linear array
     RawData = np.fromstring(Raw, Info['DataDType'])
@@ -46,64 +67,65 @@ def DataRead(DataFile, InfoFile, ChList=[]):
     return(Data, Info)
 
 
-def DataWrite(DataFile, InfoFile, Data, Info={}):
+def BinWrite(DataFile, Path, Data, Info={}):
     """ Write numpy array to flat interleaved binary file. Data will be 
         represented as ch1s1-ch2s1-ch3s1-...-chNs1-ch1s2-ch2s2...chNsN.
         
         Also, write a text file containing data info for data loading. """
     
-    # Get info
+    # Get info and generate path
     Info['DataShape'] = Data.shape
     Info['DataDType'] = str(Data.dtype)
+    InfoFile = ''.join(DataFile.split('.')[:-1]) + '-Info.dict'
+    
+    if Path[-1] != '/': Path = Path + '/'
+    os.makedirs(Path, exist_ok=True)
     
     # Write text info file
-    DictWrite(InfoFile, Info)
+    DictWrite(Path + InfoFile, Info)
     
     # Write interleaved data
-    with open(DataFile, 'wb') as File: File.write(Data.T.tobytes())
+    with open(Path + DataFile, 'wb') as File: File.write(Data.T.tobytes())
     
     return(None)
 
 
-def DictRead(DictFile):
-    with open(DictFile, 'r') as File: Dict = json.load(File)
-    return(Dict)
-
-
-def DictWrite(DictFile, Dict):
-    with open(DictFile, 'w') as File: json.dump(Dict, File)
-    return(None)
+### Level 2 ###
+def DataWrite(DataFile, TarFile, DataPath, Data, Info):
+    BinWrite(DataFile, DataPath, Data, Info)
+    
+    FileList = glob(DataPath + '**/*.*', recursive=True); FileList.sort()
+    
+    TarWrite(TarFile, FileList)
+    DataDir = DataPath.split('/')[0]
+    shutil.rmtree(DataDir)
 
     
 #%% Test Write/Read
-DataFile = 'Data.flat'; InfoFile = 'Info.dict'
-Info = {'ChNo': 27,
-        'Format': '<f'}
-#ChList = [1, 3, 10, 27]
-ChList = []
-
-Data = np.random.randn(Info['ChNo'], 30000)
-Data = np.array(Data, Info['Format'])
-#Data = Data.T
-
-DataWrite(DataFile, InfoFile, Data, Info)
-DataTest, InfoTest = DataRead(DataFile, InfoFile, ChList)
+#DataFile = 'Data.flat'; InfoFile = 'Info.dict'
+#Info = {'ChNo': 27,
+#        'Format': '<f'}
+##ChList = [1, 3, 10, 27]
+#ChList = []
+#
+#Data = np.random.randn(Info['ChNo'], 30000)
+#Data = np.array(Data, Info['Format'])
+##Data = Data.T
+#
+#BinWrite(DataFile, InfoFile, Data, Info)
+#DataTest, InfoTest = BinRead(DataFile, InfoFile, ChList)
 
 #%% Test WriteTar
-Path = 'Exp/Conn/Setup/'
-os.makedirs(Path, exist_ok=True)
-
-TarFile = 'Exp.tar'
-DataFile = 'Data.flat'; InfoFile = 'Info.dict'
-DataFile = Path + DataFile; InfoFile = Path + InfoFile
+Exp = 'SC-20170121-132017'; Conn = 'I-M-M-S'; Setup = 'UnitRec'
+Path = Exp + '/' + Conn + '/' + Setup + '/'
+TarFile = Exp + '.tar'
+DataFile = Exp +'.flat'
 
 Info = {'ChNo': 27, 'Format': '<f'}
 Data = np.random.randn(Info['ChNo'], 30000)
 Data = np.array(Data, Info['Format'])
 
-DataWrite(DataFile, InfoFile, Data, Info)
-FileList = glob(Path + '*', recursive=True); FileList.sort()
+DataWrite(DataFile, TarFile, Path, Data, Info)
 
-with tarfile.open(TarFile, 'a') as F:
-    for File in FileList:
-        F.add(File)
+F = tarfile.open(TarFile, 'r')
+F.getmembers()

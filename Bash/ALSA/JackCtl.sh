81ccbd6d478e8d1fbf0e900beb698c4fd035baa1
Thawann Malfatti <malfatti@neuro.ufrn.br>
2017-02-04 21:31:02 -0200

    Implement --card and --path options for JackCtl.sh

--- Bash/ALSA/JackCtl.sh
+++ Bash/ALSA/JackCtl.sh
@@ -4,12 +4,19 @@ while [[ $# -gt 1 ]]; do
     key="$1"
 
     case $key in
+        --path)
+            Path="$2"
+        ;;
         --rt)
             RT="$2"
             shift
         ;;
         --priority)
             Prio="$2"
+            shift
+        ;;
+        --card)
+            Card="$2"
             shift
         ;;
         --rate)
@@ -35,28 +42,36 @@ Catch=30000
 ResamplingRate=48000
 ResamplingQuality=1
 
-echo "Jack options: -"$RT" -P"$Prio" -dalsa -dhw:PCH -r"$Rate" -p"$PeriodSize" -n"$Periods"" > ~/.log/JackSession.log &
+echo "Jack path: "$Path"" > ~/.log/JackSession.log &
+echo "Jack options: jackd -"$RT" -P"$Prio" -dalsa -dhw:"$Card" -r"$Rate" -p"$PeriodSize" -n"$Periods"" >> ~/.log/JackSession.log &
+echo "" >> ~/.log/JackSession.log &
 
-jackd -"$RT" -P"$Prio" -dalsa -dhw:PCH -r"$Rate" -p"$PeriodSize" -n"$Periods" &>> ~/.log/JackSession.log &
+"$Path"jackd -"$RT" -P"$Prio" -dalsa -dhw:"$Card" -r"$Rate" -p"$PeriodSize" -n"$Periods" &>> ~/.log/JackSession.log &
 
 sleep 2
 
 echo "Building output bridge..." >> ~/.log/JackSession.log &
 echo "alsa_out options:  -j ALSAInput -dALSAOutput1 -f "$Catch" -q "$ResamplingQuality" -r "$ResamplingRate" -p "$PeriodSize" -n "$Periods"" &>> ~/.log/JackSession.log &
-/usr/bin/alsa_out -j ALSAInput -dALSAOutput1 -f "$Catch" -q "$ResamplingQuality" -r "$ResamplingRate" -p "$PeriodSize" -n "$Periods" &>> ~/.log/JackSession.log &
-#/usr/bin/alsa_out -j ALSAInput -dALSAOutput1 &>> ~/.log/JackSession.log &
+echo "" >> ~/.log/JackSession.log &
+
+"$Path"alsa_out -j ALSAInput -dALSAOutput1 -f "$Catch" -q "$ResamplingQuality" -r "$ResamplingRate" -p "$PeriodSize" -n "$Periods" &>> ~/.log/JackSession.log &
+#"$Path"alsa_out -j ALSAInput -dALSAOutput1 &>> ~/.log/JackSession.log &
 
 echo "Building input bridge..." >> ~/.log/JackSession.log &
 echo "alsa_in options: -j ALSAOutput -dALSAInput1 -f "$Catch" -q "$ResamplingQuality" -r "$ResamplingRate" -p "$PeriodSize" -n "$Periods"" &>> ~/.log/JackSession.log &
-/usr/bin/alsa_in -j ALSAOutput -dALSAInput1 -f "$Catch" -q "$ResamplingQuality" -r "$ResamplingRate" -p "$PeriodSize" -n "$Periods" &>> ~/.log/JackSession.log &
-#/usr/bin/alsa_in -j ALSAOutput -dALSAInput1 &>> ~/.log/JackSession.log &
+echo "" >> ~/.log/JackSession.log &
+
+"$Path"alsa_in -j ALSAOutput -dALSAInput1 -f "$Catch" -q "$ResamplingQuality" -r "$ResamplingRate" -p "$PeriodSize" -n "$Periods" &>> ~/.log/JackSession.log &
+#"$Path"alsa_in -j ALSAOutput -dALSAInput1 &>> ~/.log/JackSession.log &
 
 sleep 2
 
 echo "Connecting bridge to output..." >> ~/.log/JackSession.log &
-jack_connect ALSAOutput:capture_1 system:playback_1
-jack_connect ALSAOutput:capture_2 system:playback_2
+"$Path"jack_connect ALSAOutput:capture_1 system:playback_1
+"$Path"jack_connect ALSAOutput:capture_2 system:playback_2
 
 echo "Connecting bridge to input..." >> ~/.log/JackSession.log &
-jack_connect system:capture_1 ALSAInput:playback_1
-jack_connect system:capture_2 ALSAInput:playback_2
+"$Path"jack_connect system:capture_1 ALSAInput:playback_1
+"$Path"jack_connect system:capture_2 ALSAInput:playback_2
+
+echo "" >> ~/.log/JackSession.log &
--- Bash/ALSA/SoundCtl.sh
+++ Bash/ALSA/SoundCtl.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
+#JackPath="/home/malfatti/Software/Programs/Jack1/bin/"
+JackPath="/usr/bin/"
+
 if [ "${1,,}" == hdmi ]; then
     killall jackd &> /dev/null
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/HDMI ~/.asoundrc
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/HDMIVol.sh ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/VolControl.sh
+
 elif [ "${1,,}" == analog ]; then
     killall jackd &> /dev/null
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/Analog ~/.asoundrc
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/PCHVol.sh ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/VolControl.sh
+
 elif [ "${1,,}" == jack ]; then
     killall jackd &> /dev/null
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/Jack-ALSA ~/.asoundrc
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/PCHVol.sh ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/VolControl.sh
-    bash ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/JackCtl.sh --rt r --rate 48000 --periodsize 1024 --periods 3 --priority 19
+    bash ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/JackCtl.sh --path "$JackPath" --rt r --card PCH --rate 48000 --periodsize 1024 --periods 3 --priority 19
+
 elif [ "${1,,}" == jackrt ]; then
     killall jackd &> /dev/null
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/Jack-ALSA ~/.asoundrc
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/PCHVol.sh ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/VolControl.sh
-    bash ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/JackCtl.sh --rt R --rate 192000 --periodsize 384 --periods 3 --priority 89
+    bash ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/JackCtl.sh --path "$JackPath" --rt R --card PCH --rate 192000 --periodsize 384 --periods 3 --priority 89
+
+elif [ "${1,,}" == jackusbpre2 ]; then
+    killall jackd &> /dev/null
+    cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/Jack-ALSA ~/.asoundrc
+    cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/PCHVol.sh ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/VolControl.sh
+    bash ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/JackCtl.sh --path "$JackPath" --rt R --card USBPre2 --rate 192000 --periodsize 384 --periods 3 --priority 89
+
 elif [ "${1,,}" == spdif ]; then
     killall jackd &> /dev/null
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/SPDIf ~/.asoundrc
     cp ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/PCHVol.sh ~/Software/Git/Malfatti/SciScripts/Bash/ALSA/VolControl.sh
+
 else
-    echo "Usage: SoundCtl [hdmi | analog | jack | jackrt | spdif]"
+    echo "Usage: SoundCtl [hdmi | analog | jack | jackrt | jackusbpre2 | spdif]"
 fi

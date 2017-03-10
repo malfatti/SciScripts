31863295f5bdef3a9bd19e7a8f8402b2e108c828
malfatti <malfatti@neuro.ufrn.br>
2017-03-10 18:18:59 -0300

    fix NetCtl

--- BeagleBone/Bash/NetCtl.sh
+++ BeagleBone/Bash/NetCtl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "${1,,}" == dhcp ]; then
-    cp $SCRIPTSPATH/BeagleBone/Bash/netDHCP /etc/conf.d/net
+    cp /etc/conf.d/netDHCP /etc/conf.d/net
     /etc/init.d/net.eth0 restart
 elif [ "${1,,}" == home ]; then
-    cp $SCRIPTSPATH/BeagleBone/Bash/netHome /etc/conf.d/net
+    cp /etc/conf.d/netHome /etc/conf.d/net
     /etc/init.d/net.eth0 restart
 elif [ "${1,,}" == ice ]; then
-    cp $SCRIPTSPATH/BeagleBone/Bash/netICe /etc/conf.d/net
+    cp /etc/conf.d/netICe /etc/conf.d/net
     /etc/init.d/net.eth0 restart
 else
     echo "Usage: "$0" [dhcp | home | ice]"

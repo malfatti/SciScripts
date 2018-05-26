7799ad7d441cecd588a619073655bba780507f22
Thawann Malfatti <malfatti@neuro.ufrn.br>
2018-05-26 12:32:04 -0300

    Add walpaper fix for DisplayToggle

--- Bash/Power/GoToSleep.sh
+++ Bash/Power/GoToSleep.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
+echo "Locking screen..."; slock &
 echo "Stopping Jack...";
 killall jackd &> /dev/null; killall alsa_in &> /dev/null
-#echo "Stopping Bumblebee..."; killall bumblebeed
-#echo "Stopping Alsa..."; sudo /etc/init.d/alsasound stop &> /dev/null
 echo "Suspending..."; sudo pm-suspend
 
 echo "Waking..."
-echo "Locking screen..."; slock &
-#echo "Starting Alsa..."; sudo /etc/init.d/alsasound start &> /dev/null
-#echo "Starting Bumblebee..."; sudo bumblebeed &>> ~/Software/Git/Malfatti/SciScripts/Bash/BumblebeeSession.log
 echo "Starting Jack...";
 bash ~/Software/Git/Malfatti/SciScripts/Bash/Audio/ALSA/SoundAutostart.sh &> /dev/null
 echo "Awake."
--- Bash/Video/DisplayToggle.sh
+++ Bash/Video/DisplayToggle.sh
@@ -8,11 +8,13 @@ if xrandr | grep "$HDMI connected"; then
     if [ $State == "OutOff" ]; then
         xrandr --output "$HDMI" --auto
         xrandr --output "$HDMI" --right-of "$Notebook"
+        feh --bg-scale --randomize ~/Images/Others/Wallpapers/*
         echo "Extended" > $StateFile
     
     elif [ $State == "Extended" ]; then
         xrandr --output "$HDMI" --auto
         xrandr --output "$HDMI" --same-as "$Notebook"
+        feh --bg-scale --randomize ~/Images/Others/Wallpapers/*
         echo "Mirror" > $StateFile
     
     #elif [ $State == "Mirror" ]; then
@@ -23,9 +25,11 @@ if xrandr | grep "$HDMI connected"; then
     else
         xrandr --output "$Notebook" --auto
         xrandr --output "$HDMI" --off
+        feh --bg-scale --randomize ~/Images/Others/Wallpapers/*
         echo "OutOff" > $StateFile
     fi
 else
     xrandr --output "$HDMI" --off
+    feh --bg-scale --randomize ~/Images/Others/Wallpapers/*
     echo "OutOff" > $StateFile
 fi
